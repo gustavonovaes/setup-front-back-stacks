3dba21981d460737e8ce42d124e985879636d4a8
Gustavo Novaes <gustavonovaes.dev@gmail.com>
2025-07-22 13:39:11 -0300

    feat: atualiza cenario de teste k6 para 3 estagios

--- k6.js
+++ k6.js
@@ -2,8 +2,19 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  scenarios: {
+    contacts: {
+      executor: 'ramping-arrival-rate',
+      preAllocatedVUs: 10,
+      timeUnit: '1s',
+      startRate: 50,
+      stages: [
+        { target: 200, duration: '10s' }, // linearly go from 50 iters/s to 200 iters/s for 30s
+        { target: 500, duration: '0' }, // instantly jump to 500 iters/s
+        { target: 500, duration: '2m' }, // continue with 500 iters/s for 10 minutes
+      ],
+    },
+  },
 };
 
 export default function () {
